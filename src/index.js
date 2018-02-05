99ad161226e7581a2b93520f41c35c48e0e91497
DRinsvind <adskiyprikol@gmail.com>
2018-02-05 21:42:02 +0200

    some wrong

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import Root from './components/Root'
+import App from './App'
+import React,{Component} from 'react'
+import {render} from 'react-dom'
+
+
+const wrapper = document.getElementsByClassName('games__layout')[0]
+console.log(wrapper)
+
+// render(<App/>,wrapper)
